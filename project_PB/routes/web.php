a6dc4868e490899cee1c37938ff3f382fcf9ac4b
AlexK85 <saharr.k@mail.ru>
2022-04-12 20:06:04 +0300

    Именованное отслеживание URL адресов. И вывод массив сообщений формы

--- project_PB/routes/web.php
+++ project_PB/routes/web.php
@@ -1,20 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
 
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('contact');
 
-Route::post('/contact/submit', function () {
-    return "OKEY";
+Route::post('/contact/submit', function (Request $request) {
+    // Illuminate\Http\Request::all();
+    // Request::all();
+    dd($request->all());
 })->name('contact-form');
