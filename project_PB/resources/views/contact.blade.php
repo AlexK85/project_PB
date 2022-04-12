065c4b647cf69b1ab4f3dfbf6555e8bcd9cc09e5
AlexK85 <saharr.k@mail.ru>
2022-04-12 19:39:16 +0300

    Добавление формы

--- project_PB/resources/views/contact.blade.php
+++ project_PB/resources/views/contact.blade.php
@@ -4,5 +4,29 @@
 
 @section('content')
     <h1>Страница контактов</h1>
+
+    <form action="/contact/submit" method="post">
+        @csrf
+
+        <div class="form-group">
+            <label for="name">Введите имя</label>
+            <input type="text" name="name" id="name" placeholder="Введите имя" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="email ">Email</label>
+            <input type="text" name="email" id="email" placeholder="Введите email" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="subject">Тема сообщения</label>
+            <input type="text" name="subject" id="subject" placeholder="Тема сообщения" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="message">Cообщение</label>
+            <textarea name="message" id="message" class="form-control" placeholder="Введите сообщение"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-success">Отправить</button>
+    </form>
+
 @endsection
 
